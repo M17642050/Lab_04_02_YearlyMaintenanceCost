5d3aca7e71a3124da27d9c7618d3ef1b607eacd8
rudra-prajapati <prajapra@mail.uc.edu>
2026-09-18 09:36:09 -0400

    Task 02 commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main (String[] args) {
+        double springCost = 553.5;
+        double summerCost = 777.0;
+        double fallCost = 450.21;
+        double winterCost = 307.68;
+        double yearlyCost = springCost + summerCost + fallCost + winterCost;
+        System.out.println("The seasonal home maintenance costs for Spring: " + springCost);
+        System.out.println("The seasonal home maintenance costs for Summer: " + summerCost);
+        System.out.println("The seasonal home maintenance costs for Fall: " + fallCost);
+        System.out.println("The seasonal home maintenance costs for Winter: " + winterCost);
+        System.out.println("The total yearly home maintenance costs: " + yearlyCost);
+    }
 }
